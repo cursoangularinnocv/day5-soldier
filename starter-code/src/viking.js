05f7f0f135f5bae028d8e261dab1a1bdde6d9674
Daniel Vicario <danigv.email@gmail.com>
2019-04-08 10:22:19 +0200

    added examples

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,40 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    xAxisAdvance = 10
 
-// Viking
-class Viking {}
+    constructor (health = 120, strength = 120) {
+        this.x = 100
+        this.y = 200
+        this.health = health;
+        this.strength = strength;
 
-// Saxon
-class Saxon {}
+        if (health < 0) {
+            this.health = 100
+        }
 
-// War
-class War {}
+        if (strength < 0) {
+            this.strength = 100
+        }
+    }
+
+    die() {
+        this.health = 0
+    }
+
+    getData() {
+        return {
+            x: this.x,
+            y: this.y,
+            strength: this.strength,
+            health: this.health
+        }
+    }
+
+    moveLeft() {
+        this.x -= this.xAxisAdvance
+    }
+
+    moveRight() {
+        this.x += this.xAxisAdvance
+    }
+}
